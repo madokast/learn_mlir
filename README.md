6051ced46def5e50f872522faf624e2a740af07d
madokast <578562554@qq.com>
2025-07-20 07:55:22 +0000

    convert onnx

--- README.md
+++ README.md
@@ -1,16 +1,50 @@
 # learn_mlir
 学习 MLIR
 
-# 外部仓库
+# 环境准备
 
-## torch-mlir
+sudo apt update
 
-git clone https://github.com/llvm/torch-mlir.git
+## python3.11
 
-## llvm-project
+安装 pyenv `curl https://pyenv.run | bash`
 
-git clone --recursive https://github.com/llvm/llvm-project.git
+按要求添加到 `~/.bashrc`
 
-cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;mlir;polly" -G "Unix Makefiles" ../llvm
+```
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
+eval "$(pyenv virtualenv-init -)"
+```
+
+加载 `source ~/.bashrc`
+
+下载 python 3.11.13 `pyenv install 3.11.13`
+
+查看已安装的版本 `pyenv versions`
+
+全局切换 `pyenv global 3.11.13`
+
+检查 python 版本 `python --version`
+
+## torch 导出 ONNX
+
+虚拟环境 `python -m venv venv`
+
+激活 `source ./venv/bin/activate`
+
+检测，安装
+
+```
+pip3 install torch torchvision torchaudio
+pip install --upgrade onnx onnxscript
+```
+
+运行 `a01_hello_mlir/torch_mod.py` 可以得到 onnx 模型
+
+通过 netron.app 可以查看模型结构
+
+## ONNX 导出 MLIR
 
 
--- a01_hello_mlir/torch_mod.py
+++ a01_hello_mlir/torch_mod.py
@@ -1,17 +1,18 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.version
 
 default_mod_weight_path = 'torch_mod_w.pt'
+default_onnx_path = 'torch_mod.onnx'
 
 class MyModel(nn.Module):
     def __init__(self, input_dim:int=4, hid_dim:int=16, bias:bool=True):
         super().__init__()
+        self.input_dim = input_dim
         self.fc1 = nn.Linear(input_dim, hid_dim, bias=bias)
         self.fc2 = nn.Linear(hid_dim, input_dim, bias=bias)
 
-    def forward(self, x):
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
@@ -24,11 +25,39 @@ class MyModel(nn.Module):
     def load_weight(self, path=default_mod_weight_path):
         self.load_state_dict(torch.load(path))
 
+    def example_input(self, *prefix_shape:int) -> torch.Tensor:
+        """
+        样例输入 model.example_input(2, 3) -> (2, 3, input_dim)
+        """
+        shape = list(prefix_shape) + [self.input_dim]
+        return torch.rand(*shape)
+
+    def to_onnx(self, example_input:torch.Tensor, path=default_onnx_path):
+        # pip install --upgrade onnx onnxscript
+        self.eval()
+        torch.onnx.export(
+            model=self,
+            args=(example_input, ),
+            f = path,
+            input_names=['input'],
+            output_names=['output'],
+            dynamo=True
+        )
+
+
+def fix_seed(seed:int):
+    import random
+    import numpy as np
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.random.manual_seed(seed) 
+
 if __name__ == '__main__':
     import os
 
     model = MyModel()
     if not os.path.exists(default_mod_weight_path):
+        fix_seed(1)
         model.save_weight()
     model.load_weight()
     model.eval()
@@ -39,6 +68,11 @@ if __name__ == '__main__':
     ])
     y = model(x)
 
-    print(torch.__version__) # 2.7.1+cu126
+    print(torch.__version__) # 2.9.0.dev20250719+cpu
     print(x)
+
+    # tensor([[0.2262, 0.2880, 0.2197, 0.2661],
+    #         [0.2218, 0.3190, 0.1965, 0.2627]], grad_fn=<SoftmaxBackward0>)
     print(y)
+
+    model.to_onnx(x)
