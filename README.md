13255578e1d5b88ea1aad5c07c8f563e463124b0
madokast <578562554@qq.com>
2025-07-22 00:14:59 +0800

    do well in onnx-mlir

--- README.md
+++ README.md
@@ -3,9 +3,16 @@
 
 # 环境准备
 
+```
 sudo apt update
 
-## python3.11 可跳过直接用3.12
+sudo apt install -y build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev wget curl \
+  libncursesw5-dev xz-utils libxml2-dev libxmlsec1-dev \
+  libffi-dev liblzma-dev
+```
+
+## python3.11 这个和LLVM最相应
 
 安装 pyenv `curl https://pyenv.run | bash`
 
@@ -20,13 +27,17 @@ eval "$(pyenv virtualenv-init -)"
 
 加载 `source ~/.bashrc`
 
-下载 python 3.11.13 `pyenv install 3.11.13`
+下载 python 3.11.13 `pyenv install 3.11.13`（作废）
+
+注意我们需要编译为动态库 `env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.11.13`
 
 查看已安装的版本 `pyenv versions`
 
 全局切换 `pyenv global 3.11.13`
 
 检查 python 版本 `python --version`
+
+使用 `pyenv which python` 查看 python 位置，后续 DPython3_ROOT_DIR 会用到
 
 ## onnx-mlir 安装
 
@@ -36,7 +47,16 @@ https://github.com/onnx/onnx-mlir/blob/main/docs/BuildOnLinuxOSX.md 安装手册
 
 安装 ninja `sudo apt install -y ninja-build`
 
-需要 `libprotobuf-dev` 先直接安装试试
+需要 `libprotobuf-dev` 先直接安装试试。失败了，版本过低，可以直接下载预编译版本
+
+```
+wget https://github.com/protocolbuffers/protobuf/releases/download/v31.1/protoc-31.1-linux-x86_64.zip
+# 解压后添加到 PATH
+export PATH=/home/mdk/repo/learn_mlir/protoc-31.1/bin:$PATH
+
+```
+
+还是先要编译 LLVM，具体见手册
 
 ```
 cmake -G Ninja ../llvm \
@@ -53,6 +73,47 @@ cmake --build . --target check-mlir
 ```
 
 报错 /usr/bin/ld: /home/codespace/.python/current/lib/libpython3.12.a(import.o): relocation R_X86_64_TPOFF32 against hidden symbol `pkgcontext' can not be used when making a shared object
+
+因为 libpython3 没有动态库，见 pyenv 安装 python
+
+下面安装 onnx-mlir
+
+```
+# 指定 MLIR 的位置
+git clone --recursive https://github.com/onnx/onnx-mlir.git 
+MLIR_DIR=/home/mdk/repo/learn_mlir/llvm-project/build_4_onnx_mlir/lib/cmake/mlir
+pythonLocation=/home/mdk/.pyenv/versions/3.11.13
+mkdir onnx-mlir/build && cd onnx-mlir/build
+cmake -G Ninja \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DLLVM_ENABLE_ASSERTIONS=ON \
+      -DPython3_ROOT_DIR=${pythonLocation} \
+      -DMLIR_DIR=${MLIR_DIR} \
+      ..
+
+cmake --build .
+
+# Run lit tests:
+export LIT_OPTS=-v
+cmake --build . --target check-onnx-lit
+```
+
+编译报错，可能是 GCC 版本太高了（确实啊）
+```
+sudo apt install gcc-9 g++-9
+
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 100
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 100
+
+sudo update-alternatives --config gcc
+sudo update-alternatives --config g++
+
+# 恢复
+
+sudo update-alternatives --auto gcc
+sudo update-alternatives --auto g++
+```
 
 ## IREE 安装
 
--- a01_hello_mlir/torch_mod.py
+++ a01_hello_mlir/torch_mod.py
@@ -6,18 +6,26 @@ default_mod_weight_path = 'torch_mod_w.pt'
 default_onnx_path = 'torch_mod.onnx'
 
 class MyModel(nn.Module):
-    def __init__(self, input_dim:int=4, hid_dim:int=16, bias:bool=True):
+    def __init__(self, seq_len:int=4096, vocab_size:int=32000, 
+            embed_dim:int=256, hid_dim:int=512, bias:bool=True,
+            temperature:float=0.7):
         super().__init__()
-        self.input_dim = input_dim
-        self.fc1 = nn.Linear(input_dim, hid_dim, bias=bias)
-        self.fc2 = nn.Linear(hid_dim, input_dim, bias=bias)
+        self.seq_len = seq_len
+        self.temperature = temperature
+        self.em = nn.Embedding(vocab_size, embed_dim)
+        self.fc1 = nn.Linear(embed_dim, hid_dim, bias=bias)
+        self.fc2 = nn.Linear(hid_dim, vocab_size, bias=bias)
 
     def forward(self, x:torch.Tensor) -> torch.Tensor:
-        x = self.fc1(x)
+        x = self.em(x) # (batch, len, embed_dim)
+        x = self.fc1(x) # (batch, len, hid_dim)
         x = F.relu(x)
-        x = self.fc2(x)
-        x = F.softmax(x, dim=-1)
-        return x
+        logits = self.fc2(x) # (batch, len, vocab_size)
+        last_logits = logits[:, -1, :] # (batch, vocab_size)
+        scaled_logits = last_logits / self.temperature
+        probabilities = F.softmax(scaled_logits, dim=-1)
+        sampled_tokens = torch.argmax(probabilities, dim=-1) # (batch,)
+        return sampled_tokens
 
     def save_weight(self, path=default_mod_weight_path):
         torch.save(self.state_dict(), path)
@@ -25,24 +33,26 @@ class MyModel(nn.Module):
     def load_weight(self, path=default_mod_weight_path):
         self.load_state_dict(torch.load(path))
 
-    def example_input(self, *prefix_shape:int) -> torch.Tensor:
+    def example_input(self, batch_size:int) -> torch.Tensor:
         """
-        样例输入 model.example_input(2, 3) -> (2, 3, input_dim)
+        样例输入 (batch_size, seq_len)
         """
-        shape = list(prefix_shape) + [self.input_dim]
-        return torch.rand(*shape)
+        token_ids = torch.randint(0, vocab_size, (batch_size, self.seq_len))
+        return token_ids
 
     def save_onnx(self, example_input:torch.Tensor, path=default_onnx_path):
         # pip install --upgrade onnx onnxscript
         import os
         if not os.path.exists(path):
             self.eval()
-            torch.onnx.export(
-                model=self,
-                args=(example_input, ),
-                f = path,
-                dynamo=True
-            )
+            # torch.onnx.export(
+            #     model=self,
+            #     args=(example_input, ),
+            #     f = path,
+            #     dynamo=True
+            # )
+            onnx_program = torch.onnx.export(self, (example_input, ), dynamo=True)
+            onnx_program.save(path)
 
 
 def fix_seed(seed:int):
@@ -63,13 +73,12 @@ if __name__ == '__main__':
     model.eval()
 
     x = torch.Tensor([
-        [0.1, 0.2, 0.3, 0.4],
-        [0.5, 0.6, 0.7, 0.8]
-    ])
+        [0] * (4096 - 4) + [1,2,3,4],
+        [0] * (4096 - 4) + [5,6,7,8]
+    ]).type(torch.int64)
     print("input", x, sep='\n')
 
-    # tensor([[0.2262, 0.2880, 0.2197, 0.2661],
-    #         [0.2218, 0.3190, 0.1965, 0.2627]], grad_fn=<SoftmaxBackward0>)
+    # tensor([12971, 31737])
     print("torch infer", model(x), sep='\n')
 
     model.save_onnx(example_input=x)
@@ -77,8 +86,7 @@ if __name__ == '__main__':
     from onnx_infer import infer
 
     print("onnx infer")
-    # [array([[0.22616413, 0.28796327, 0.21973658, 0.26613602],
-    #   [0.22183387, 0.3190139 , 0.1964965 , 0.26265574]], dtype=float32)]
+    # [array([12971, 31737], dtype=int64)]
     print(infer(
         path=default_onnx_path,
         input_data=x.numpy()
